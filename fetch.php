8f16d6130873079d9629235c4eabc2e6f95eed69
murimi-cpu <murimi.james.mj@gmail.com>
2024-02-27 18:16:42 +0300

    php_assignments

---
+++ fetch.php
@@ -0,0 +1,21 @@
+<?php
+$servername = "localhost";
+$username ="root";
+$password = "";
+$database = "school";
+
+$conn =mysqli_connect($servername, $username, $password, $database);
+if($conn){
+    $sql = "";
+    echo"data fetched successfull!" . ;
+}
+else{
+
+}
+
+
+
+
+
+
+?>
