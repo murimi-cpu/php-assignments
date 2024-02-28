8eac17e35527f11671a3560db5083ec775c26e27
murimi-cpu <murimi.james.mj@gmail.com>
2024-02-28 22:51:10 +0300

    php_assigments

--- fetch.php
+++ fetch.php
@@ -6,13 +6,15 @@ $database = "school";
 
 $conn =mysqli_connect($servername, $username, $password, $database);
 if($conn){
-    $sql = "";
-    echo"data fetched successfull!" . ;
+    $sql = "SELECT * FROM `tblstudents` ORDER BY `tblstudents`.`stid` = 1 ";
+    $result = mysqli_query($conn, $sql);
+    echo"data fetched successfull!";
 }
 else{
+echo"connection failed". mysqli_connect_error();
 
 }
-
+mysqli_close($conn);
 
 
 
