d2c1f22d3366391a2217167826a0bf9997f49dd5
murimi-cpu <murimi.james.mj@gmail.com>
2024-03-11 22:52:45 +0300

    register

--- register.php
+++ register.php
@@ -13,16 +13,16 @@ if(!$conn){
 }
 else{
         // use superglobal variable to check request method and allow code to run if true
-    if( $_SERVER['REQUEST_METHOD'] == 'POST' )
+    if( $_SERVER["REQUEST_METHOD"] == "POST" )
         // sanitinze the input to avoid crossscripting injections for security
-        $name = htmlspecialchars($_POST['name'];)
-        $email = htmlspecialchars($_POST['email'];)
-        $phone= htmlspecialchars($_POST['phone'];)
-        $address= htmlspecialchars($_POST['address'];)
-        $password = htmlspecialchars($_POST['password'];)
+        $full_name = htmlspecialchars($_POST["full_name"]);
+        $email = htmlspecialchars($_POST["email"]);
+        $phone= htmlspecialchars($_POST["phone"]);
+        $address= htmlspecialchars($_POST["address"]);
+        $password = htmlspecialchars($_POST["password"]);
 
         // insert date into tbluser
-        $sql = "INSERT INTO `tbluser` (`full_name`, `email`, `phone`, `address`, `password`) VALUES ('Murimi James', 'murimi@gmail.com', '0725945079', '40400-63', '$2y$10$bx1dd9u9Z1R0tG9gM.HQWu/wUdJqgG165kLzWT3gBxdpd5oa0wdMO');";
+        $sql = "INSERT INTO `tbluser` (`full_name`, `email`, `phone`, `address`, `password`) VALUES ('Murimi James', 'murimi@gmail.com', '0725945079', '40400-63', '$2y$10$bx1dd9u9Z1R0tG9gM.HQWu/wUdJqgG165kLzWT3gBxdpd5oa0wdMO')";
         $result = mysqli_query($conn,$sql);
 
         if($result){
